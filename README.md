f103006b10d5c20fb5a88a952b8dea2dfe85a311
freewind <nowindlee@gmail.com>
2019-05-08 20:54:51 +0800

    demo

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-TypeScript Jasmine Demo
-===========================
+TypeScript Jasmine "beforeAll" with Async Demo
+===============================================
 
-让Jasmine支持typescript代码： https://stackoverflow.com/a/49058732/342235
+如果在beforeAll里是一个async，其它的测试会不会等到它完成之后才开始？
+
+答案：是的。
 
 ```
 npm install
--- spec/hello-spec.ts
+++ spec/hello-spec.ts
@@ -1,7 +1,24 @@
 import hello from '../hello';
 
+function wait(ms: number): Promise<void> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  })
+}
+
+let token: string = '';
+
+beforeAll(async () => {
+  await wait(1000);
+  token = 'go!'
+});
+
+
 describe("A suite", function () {
   it("contains spec with an expectation", function () {
-    expect(hello()).toBe("Hello,world!");
+    expect(hello()).toEqual("Hello,world!");
+    expect(token).toEqual('go!')
   });
 });
